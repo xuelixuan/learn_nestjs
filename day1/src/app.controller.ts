b011e5961773137b2a62d65430ca00c2084a3114
imhan <searchi@163.com>
2022-03-24 22:37:10 +0800

    day1

--- day1/src/app.controller.ts
+++ day1/src/app.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +16,31 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/users')
+  getUsers(): any {
+    return this.appService.getUsers();
+  }
+
+  @Get('/user/:id')
+  getUser(@Param('id') id: string): any {
+    return this.appService.getUser(id);
+  }
+
+  @Post('/user')
+  postUser(@Body() user): any {
+    user.id = this.appService.getUsers().length + 1;
+    return this.appService.postUser(user);
+  }
+
+  @Post('/user/:id')
+  putUser(@Body() user): any {
+    return this.appService.putUser(user);
+  }
+
+  @Delete('/user/:id')
+  deleteUser(@Param('id') id: string): any {
+    return this.appService.deleteUser(id);
   }
 }
--- day1/src/app.service.ts
+++ day1/src/app.service.ts
@@ -1,8 +1,53 @@
 import { Injectable } from '@nestjs/common';
+import { filter } from 'rxjs';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  // 查询列表数据
+  getUsers(): any[] {
+    return [
+      { id: 1, name: 'zhangsan', age: 14 },
+      { id: 2, name: 'wangwu', age: 21 },
+    ];
+  }
+
+  // 查询一个用户
+  getUser(id): any {
+    let users = this.getUsers();
+    return users.filter((user) => {
+      return user.id === parseInt(id);
+    });
+  }
+
+  // 增加一个用户
+  postUser(user): any {
+    let users = this.getUsers();
+    return users.concat([], user);
+  }
+
+  // 更新一个用户
+  putUser(user): any {
+    let [oneUser] = this.getUser(user.id);
+    oneUser.name = user.name;
+    oneUser.age = user.age;
+
+    let users = this.getUsers();
+    let filterUser = users.filter((user) => {
+      return user.id !== oneUser.id;
+    });
+    filterUser.push(oneUser);
+    return filterUser;
+  }
+
+  // 删除一个用户
+  deleteUser(id): any {
+    let users = this.getUsers();
+    return users.filter((user) => {
+      return user.id !== parseInt(id);
+    });
   }
 }
